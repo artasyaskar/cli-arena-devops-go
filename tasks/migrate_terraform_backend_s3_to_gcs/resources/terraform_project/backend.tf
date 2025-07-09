ffe2ef5218734320a855170d3117c88cd965759b
Artas Yaskar <artasyaskar@gmail.com>
2025-07-09 17:41:51 +0500

    Create backend.tf

---
+++ tasks/migrate_terraform_backend_s3_to_gcs/resources/terraform_project/backend.tf
@@ -0,0 +1,14 @@
+# This file initially defines the S3 backend.
+# The user's task is to modify this file to use a GCS backend.
+
+terraform {
+  backend "s3" {
+    # Configuration will be dynamically inserted by setup_initial_s3_backend.sh
+    # to ensure unique bucket names for testing.
+    # bucket         = "placeholder-s3-bucket-name" # To be replaced by script
+    # key            = "terraform.tfstate"
+    # region         = "us-east-1"
+    # dynamodb_table = "placeholder-dynamo-table-name" # To be replaced
+    # encrypt        = true
+  }
+}
