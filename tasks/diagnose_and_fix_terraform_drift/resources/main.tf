4cb86b02496216021baa37c7ce44ac2bf8d5380f
Artas Yaskar <artasyaskar@gmail.com>
2025-07-09 22:28:45 +0500

    Update main.tf

--- tasks/diagnose_and_fix_terraform_drift/resources/main.tf
+++ tasks/diagnose_and_fix_terraform_drift/resources/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "random_id" "bucket_suffix" {
   byte_length = 4
 }
