d32b14fde04c08cdfa16fa978245ee77f1cea820
Artas Yaskar <artasyaskar@gmail.com>
2025-07-09 22:25:52 +0500

    Update variables.tf

--- tasks/configure_cross_account_iam_role_terraform/resources/cicd_account_infra/variables.tf
+++ tasks/configure_cross_account_iam_role_terraform/resources/cicd_account_infra/variables.tf
@@ -4,20 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "cicd_account_id" {
-  description = "AWS Account ID of the CI/CD account (source)"
-  type        = string
-  default     = "111111111111" # Mocked CI/CD Account ID
-}
-
-variable "cicd_principal_name" {
-  description = "Name of the IAM user/role in the CI/CD account that will assume the role in the target account (e.g., 'cicd_user')"
-  type        = string
-  default     = "cicd_user" # This is the entity to attach the policy to.
-                            # For this task, we assume this user/role already exists or is simulated.
-                            # The Terraform config will create a policy and (optionally) attach it.
-}
-
 variable "target_deploy_role_arn" {
   description = "ARN of the 'CICDDeployRole' in the Target account (output from target_account_infra)"
   type        = string
